e1c95f8082ba3ac0afa924edebb58703674f22a4
Adam <35642115+adamJacewicz@users.noreply.github.com>
2023-12-01 12:34:33 +0100

    Refactor (#6)
    * refactor

--- src/components/Tile.vue
+++ src/components/Tile.vue
@@ -1,14 +1,16 @@
 <template>
-  <div :style="position" :class="tileClasses">
+  <div :style="position"
+       class="w-1/4 h-1/4 p-1 absolute xs:text-5xl font-bold duration-200">
     <div
       ref="tileInner"
-      :class="innerClasses"
+      :class="[colorClass, 'inner flex h-full items-center rounded-md justify-center text-3xl xs:text-5xl']"
     >
       {{ tileValue }}
     </div>
   </div>
 </template>
-<script setup lang="ts">
+<script setup
+        lang="ts">
 import { Tile } from "../game.types"
 import { computed, ref, watch } from "vue"
 import { popKeyframes } from "../constants"
@@ -17,46 +19,30 @@ const tileInner = ref<HTMLDivElement>()
 const props = defineProps<{ tile: Tile }>()
 const tileValue = computed(() => props.tile.value)
 
-const tileClasses = computed(() => [
-  tileValue.value < 8 ? "text-primary-800" : "text-gray-100",
-  "tile",
-  "absolute",
-  "text-4xl",
-  "xs:text-5xl",
-  "font-bold",
-  "duration-200",
-])
-
-const innerClasses = computed(() => {
+const colorClass = computed(() => {
   const exp = Math.log2(tileValue.value) % 11
   const value = Math.pow(2, exp)
-  return [
-    `bg-tile-${value}`,
-    "inner",
-    "flex",
-    "h-full",
-    "items-center",
-    "justify-center",
-    "rounded-md",
-  ]
+  const textColor = tileValue.value < 8 ? "text-primary-800" : "text-gray-100"
+  return [`bg-tile-${value}`, textColor]
 })
 
 const position = computed(() => {
   const { merged, x, y } = props.tile
   return {
     zIndex: merged ? 0 : Math.log2(tileValue.value),
-    transform: `translate(${x * 100}%, ${y * 100}%)`,
+    transform: `translate(${x * 100}%, ${y * 100}%)`
   }
 })
 
 const runPopAnimation = () => {
   tileInner.value?.animate(popKeyframes, {
-    duration: 200,
+    duration: 200
   })
 }
 watch(tileValue, runPopAnimation, { flush: "post" })
 </script>
-<style scoped lang="scss">
+<style scoped
+       lang="scss">
 @keyframes Scale {
   0% {
     transform: scale(0);
